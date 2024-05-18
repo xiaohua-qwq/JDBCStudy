b324e300bd41b3fdd2b3f92c343f69bee906abaa
xiaohua <zuwias7@gmail.com>
2024-05-18 21:56:21 +0800

    初次提交

---
+++ src/com/atguigu/base/JDBCQuick.java
@@ -0,0 +1,40 @@
+package com.atguigu.base;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBCQuick {
+    public static void main(String[] args) throws Exception {
+        // 1.注册驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // 2.创建链接对象
+        String url = "jdbc:mysql://localhost:3306/atguigu";
+        String userName = "root";
+        String password = "123456";
+        Connection connection = DriverManager.getConnection(url, userName, password);
+
+        // 3.获取执行sql语句的对象
+        Statement statement = connection.createStatement();
+
+        // 4.执行sql语句
+        String sql = "SELECT * FROM t_emp";
+        ResultSet resultSet = statement.executeQuery(sql); //得到返回的结果集
+
+        // 5.遍历结果集
+        while (resultSet.next()) {
+            int empId = resultSet.getInt("emp_id");
+            String empName = resultSet.getString("emp_name");
+            double empSalary = resultSet.getDouble("emp_salary");
+            int empAge = resultSet.getInt("emp_age");
+            System.out.println(empId + "\t" + empName + "\t" + empSalary + "\t" + empAge);
+        }
+
+        // 6.关闭链接(先开后关)
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
